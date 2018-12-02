d4f1cfe0ddf5ca0e961aebe95e8ca83784dbdfaa
Evla03 <alve@hotmail.se>
2018-12-02 14:56:51 +0100

    added day 1

---
+++ day1a.py
@@ -0,0 +1,1 @@
+﻿print(sum(eval(i) for i in open("input1.txt")))
---
+++ day1b.py
@@ -0,0 +1,12 @@
+﻿hz = 0
+i = 0
+hz_changes = [eval(i) for i in open("input1.txt")]
+hz_found = set()
+
+while True:
+    hz_found.add(hz)
+    i += 1
+    hz += hz_changes[i % len(hz_changes) - 1]
+    if hz in hz_found:
+        print(hz)
+        break
