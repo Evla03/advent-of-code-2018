63816e1f34fa7bdfb543ac02d57433044dcf9f51
Evla03 <alve@hotmail.se>
2018-12-02 16:37:09 +0100

    Create day2b.py

---
+++ day2b.py
@@ -0,0 +1,10 @@
+﻿data = [i.rstrip("\n") for i in open("input2.txt")]
+
+for i in data:
+    for j in data:
+        tmp = [1 if k != h else 0 for k, h in zip(i, j)]
+        if sum(tmp) == 1:
+            j = list(j)
+            j.pop(tmp.index(1))
+            print(''.join(j))
+            quit()
