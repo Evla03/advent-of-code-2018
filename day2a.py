df42ef42913369bfa94835a2bdd78acd6aeed733
Evla03 <alve@hotmail.se>
2018-12-02 15:27:12 +0100

    added 2a

---
+++ day2a.py
@@ -0,0 +1,10 @@
+﻿import string
+
+twos = 0
+threes = 0
+for i in open("input2.txt"):
+    tmp = [i.count(j) for j in string.ascii_lowercase]
+    twos += 1 if 2 in tmp else 0
+    threes += 1 if 3 in tmp else 0
+
+print(twos * threes)
