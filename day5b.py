30f38a5bb95eb3321d6dc49e36dfe3e9e7817c03
Alve Svarén <alve@hotmail.se>
2018-12-05 08:51:42 +0100

    Create day5b.py

---
+++ day5b.py
@@ -0,0 +1,40 @@
+import string as strs
+
+data = open("input5.txt").read()
+
+uppercase = strs.ascii_uppercase
+lowercase = strs.ascii_lowercase
+
+
+def parse(string):
+    for i in range(len(string)):
+        string = list(string)
+        tmp = string[i:i + 2]
+        tmp = ''.join(sorted(tmp))
+
+        try:
+            if tmp[0] in uppercase and tmp[1] in lowercase and tmp[0].lower() == tmp[1].lower():
+                del string[i:i + 2]
+
+        except IndexError:
+            break
+
+    return ''.join(string)
+
+
+results = {}
+old_data = None
+for i in lowercase:
+    tmp_data = data
+    tmp_data.replace(i, "")
+    tmp_data.replace(i.upper(), "")
+    print("Removed", i)
+    while tmp_data != old_data:
+        old_data = tmp_data
+        tmp_data = parse(tmp_data)
+    results[i] = len(tmp_data)
+    # print(len(data))
+    # print(data)
+
+# THIS IS REALLY, REALLY, REALLY SLOW
+print(min(results, key=lambda x: results.get(x)))
